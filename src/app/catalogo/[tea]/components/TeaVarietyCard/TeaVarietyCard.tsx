65a40a314e73d30ed2cbf94942bc0b48a9744a5d
Sara-ndonga <120710611+Sara-ndonga@users.noreply.github.com>
2024-04-22 12:27:12 +0200

    Add files via upload

--- src/app/catalogo/[tea]/components/TeaVarietyCard/TeaVarietyCard.tsx
+++ src/app/catalogo/[tea]/components/TeaVarietyCard/TeaVarietyCard.tsx
@@ -12,22 +12,12 @@ export default function TeaVarietyCard({
       className="flex flex-col justify-center items-center w-56 bg-white p-4 m-2 border rounded shadow transition-transform transform-gpu hover:scale-110"
       onClick={onClick}
     >
-      <picture>
-        <img
-          src={`/img/teas/${teaName}/${varietyName
-            .split(" ")[0]
-            .toLowerCase()}.jpeg`}
-          alt={varietyName}
-          width={128}
-          height={128}
-        />
-      </picture>
       <Image
-          src={`/img/logo.png`}
-          alt={varietyName}
-          width={128}
-          height={128}
-        />
+        src={`/img/teas/${teaName}/${varietyName.split(" ")[0].toLowerCase()}.jpeg`}
+        alt={varietyName}
+        width={128}
+        height={128}
+      />
       <p className="font-bold text-lg">{varietyName}</p>
     </button>
   );
